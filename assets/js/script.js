94eb44b23d6be9c068c715b703c79a516027f718
Ana Alves <60891489+Ajujuba@users.noreply.github.com>
2023-01-10 14:17:41 +0000

    Add animação nas composições 5 a 8 e ajuste da fonte

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,14 @@ $(document).ready(function () {
 			animeText3Scroll()
 			animeScroll4()
 			animeText4Scroll()
+			animeScroll5()
+			animeText5Scroll()
+			animeScroll6()
+			animeText6Scroll()
+			animeScroll7()
+			animeText7Scroll()
+			animeScroll8()
+			animeText8Scroll()
 		}, 500);
 	});
 });
@@ -218,6 +226,214 @@ function animeText4Scroll() {
 			$(this).addClass(comp4TextAnimationClass);
 		} else {
 			$(this).removeClass(comp4TextAnimationClass);
+		}
+	});
+
+}
+
+// ANIMAÇÃO COMPOSIÇÃO 5
+function animeScroll5() {
+	
+	
+	var comp5 = $('.comp5-animacao')
+	var comp52 = $('.comp52-animacao')
+	var comp53 = $('.comp53-animacao')
+	var comp5AnimationClass = 'comp5-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 4);
+	var documentTop = $(document).scrollTop();
+
+	comp5.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp5AnimationClass);
+		} else {
+			$(this).removeClass(comp5AnimationClass);
+		}
+	});
+	comp52.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp5AnimationClass);
+		} else {
+			$(this).removeClass(comp5AnimationClass);
+		}
+	});
+	comp53.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp5AnimationClass);
+		} else {
+			$(this).removeClass(comp5AnimationClass);
+		}
+	});
+
+}
+
+function animeText5Scroll() {
+	
+	var comp5Text = $('.comp5-text-animacao')
+	var comp5TextAnimationClass = 'comp5-text-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 6);
+	var documentTop = $(document).scrollTop();
+
+	comp5Text.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp5TextAnimationClass);
+		} else {
+			$(this).removeClass(comp5TextAnimationClass);
+		}
+	});
+
+}
+
+// ANIMAÇÃO COMPOSIÇÃO 6
+function animeScroll6() {
+	
+	
+	var comp6 = $('.comp6-animacao')
+	var comp62 = $('.comp62-animacao')
+	var comp63 = $('.comp63-animacao')
+	var comp64 = $('.comp64-animacao')
+	var comp6AnimationClass = 'comp6-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 4);
+	var documentTop = $(document).scrollTop();
+
+	comp6.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp6AnimationClass);
+		} else {
+			$(this).removeClass(comp6AnimationClass);
+		}
+	});
+	comp62.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp6AnimationClass);
+		} else {
+			$(this).removeClass(comp6AnimationClass);
+		}
+	});
+	comp63.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp6AnimationClass);
+		} else {
+			$(this).removeClass(comp6AnimationClass);
+		}
+	});
+	comp64.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp6AnimationClass);
+		} else {
+			$(this).removeClass(comp6AnimationClass);
+		}
+	});
+
+}
+
+function animeText6Scroll() {
+	
+	var comp6Text = $('.comp6-text-animacao')
+	var comp6TextAnimationClass = 'comp6-text-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 6);
+	var documentTop = $(document).scrollTop();
+
+	comp6Text.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp6TextAnimationClass);
+		} else {
+			$(this).removeClass(comp6TextAnimationClass);
+		}
+	});
+
+}
+
+// ANIMAÇÃO COMPOSIÇÃO 7
+function animeScroll7() {
+	
+	
+	var comp7 = $('.comp7-animacao')
+	var comp72 = $('.comp72-animacao')
+	var comp73 = $('.comp73-animacao')
+	var comp7AnimationClass = 'comp7-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 4);
+	var documentTop = $(document).scrollTop();
+
+	comp7.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp7AnimationClass);
+		} else {
+			$(this).removeClass(comp7AnimationClass);
+		}
+	});
+	comp72.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp7AnimationClass);
+		} else {
+			$(this).removeClass(comp7AnimationClass);
+		}
+	});
+	comp73.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp7AnimationClass);
+		} else {
+			$(this).removeClass(comp7AnimationClass);
+		}
+	});
+
+}
+
+function animeText7Scroll() {
+	
+	var comp7Text = $('.comp7-text-animacao')
+	var comp7TextAnimationClass = 'comp7-text-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 6);
+	var documentTop = $(document).scrollTop();
+
+	comp7Text.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp7TextAnimationClass);
+		} else {
+			$(this).removeClass(comp7TextAnimationClass);
+		}
+	});
+
+}
+
+// ANIMAÇÃO COMPOSIÇÃO 8
+function animeScroll8() {
+	
+	
+	var comp8 = $('.comp8-animacao')
+	var comp8AnimationClass = 'comp8-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 4);
+	var documentTop = $(document).scrollTop();
+
+	comp8.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp8AnimationClass);
+		} else {
+			$(this).removeClass(comp8AnimationClass);
+		}
+	});
+
+}
+
+function animeText8Scroll() {
+	
+	var comp8Text = $('.comp8-text-animacao')
+	var comp8TextAnimationClass = 'comp8-text-animacao-init'
+	var windowHeight = $(window).height()
+	var offset = windowHeight - (windowHeight / 6);
+	var documentTop = $(document).scrollTop();
+
+	comp8Text.each(function () {
+		if (documentTop > boxTop(this) - offset) {
+			$(this).addClass(comp8TextAnimationClass);
+		} else {
+			$(this).removeClass(comp8TextAnimationClass);
 		}
 	});
 
